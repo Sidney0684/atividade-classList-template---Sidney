2c0e059a466dabd3090ee008a831993e08d7c22d
Sidney Lopes <sidney.silvalopes@gmail.com>
2025-01-17 10:12:39 -0300

    CONCLUSÃO DA TAREFA

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 function changeStyle() {
-  //   construa aqui a sua lógica
+  const colectDivs = document.querySelectorAll(".block");
+
+  for (let i = 0; i < colectDivs.length; i++) {
+    const div = colectDivs[i];
+
+    div.classList.toggle("circle");
+  }
+
+  const listItems = document.querySelectorAll("li");
+
+  for (let i = 0; i < listItems.length; i++) {
+    const li = listItems[i];
+
+    li.classList.toggle("itemCard");
+  }
+
+  const spans = document.querySelectorAll("span");
+  const paragraphs = document.querySelectorAll("p");
+
+  for (let i = 0; i < spans.length; i++) {
+    const span = spans[i];
+    const p = paragraphs[i];
+
+    const online = span.classList.toggle("online");
+  
+    if (online) {
+      span.classList.remove("offline");
+      p.innerText = "Online";
+    } else {
+      span.classList.add("offline");
+      p.innerText = "Offline";
+    }
+  }
 }
+
+changeStyle ()
+changeStyle ()
+changeStyle ()
